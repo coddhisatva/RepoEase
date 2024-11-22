39704bd1f5e8f180241329d076c6a3b7e130b330
skaymore <conoregan151@gmail.com>
2024-11-22 11:53:00 -1000

    Sample site, locally and web hosted

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # RepoEase
 Ease Repository 0.2
+
+To deploy website:
+-locally:
+  http-server
+-web hosting:
+  firebase deploy
---
+++ public/script.js
@@ -0,0 +1,27 @@
+// Import the Firebase SDK
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, /*createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut*/ } from "firebase/auth"; // Import the auth module
+import { getFirestore } from "firebase/firestore"; // Import the Firestore module
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDwQb6wHcrjxhmukxz6CAWCdz64zwqPbv0",
+  authDomain: "ease-f14d8.firebaseapp.com",
+  projectId: "ease-f14d8",
+  storageBucket: "ease-f14d8.firebasestorage.app",
+  messagingSenderId: "1055001435382",
+  appId: "1:1055001435382:web:0074d289f778085ccda0d7",
+  measurementId: "G-RY6FXD9FZ0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Access Firebase services
+const db = getFirestore(app); // Get Firestore instance
+const auth = getAuth(app); // Get Authentication instance
+
+// ... Your Firebase code here ...
